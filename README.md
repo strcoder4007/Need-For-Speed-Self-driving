b91c68b2f4eb39a666d87e70b6b5ab719e3be507
strcoder4007 <mynameshubham23@gmail.com>
2024-07-09 14:54:45 +0530

    Edge detection working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# NFSMW-CNN-RL
+# Need For Speed Most Wanted AI using Convolution Neural Net
 Making an AI which can play Need for Speed Most Wanted only by looking at the game, just like humans learn.
+
+
+![Edge Detection](/images/nfsmw1.png)
--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import cv2
 import numpy as np
-import win32gui, win32ui, win32con, win32api
+from PIL import ImageGrab
+import time
 
 
+def process_img(original_image):
+    processed_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_image = cv2.Canny(processed_image, threshold1=150, threshold2=200)
+    return processed_image
+
+
+last_time = time.time()
 
 while True:
-    printscreen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
-    printscreen_numpy = np.array(printscreen_pil.getdata(), dtype='uint8')\
-        .reshape((printscreen_pil.size[1], printscreen_pil.size[0], 3))
-    cv2.imshow('window', printscreen_numpy)
+    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+    new_screen = process_img(screen)
+
+    print(f"Loop took {time.time() - last_time} seconds")
+
+    last_time = time.time()
+
+    cv2.imshow('window', new_screen)
+    #cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+
+
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
