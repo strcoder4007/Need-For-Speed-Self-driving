990d558af965e69b4c2acf458170276c80184002
strcoder4007 <mynameshubham23@gmail.com>
2024-07-11 11:19:39 +0530

    Stable version working with lane detection

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Need For Speed Most Wanted Self Driving using Convolution Neural Net
 
 
-<i>Work in Progress...</i>
+<h3><i>Work in Progress...</i></h3>
 
 Making an AI which can play Need for Speed Most Wanted only by looking at the game, just like humans learn.
 
 ## Detecting Lanes
-![Edge Detection](/images/nfsmw1.png)
+![Edge Detection](/images/nfsmw1.gif)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import cv2
 import numpy as np
-from numpy import ones,vstack
-from numpy.linalg import lstsq
-from statistics import mean
-from PIL import ImageGrab
 import time
-from directKeys import PressKey, ReleaseKey, W, A, S, D
+# from directKeys import PressKey, ReleaseKey, W, A, S, D
+
+from grabscreen import grab_screen
+from drawlanes import draw_lanes
 
 
 import pyautogui
@@ -27,11 +26,15 @@ def left():
     pydirectinput.keyDown('a')
     pydirectinput.keyDown('w')
     pydirectinput.keyUp('d')
+    time.sleep(0.01)
+    pydirectinput.keyUp('a')
 
 def right():
     pydirectinput.keyDown('d')
-    pydirectinput.keyUp('a')
     pydirectinput.keyDown('w')
+    pydirectinput.keyUp('a')
+    time.sleep(0.01)
+    pydirectinput.keyUp('d')
 
 def reset():
     pydirectinput.keyUp('w')
@@ -39,104 +42,35 @@ def reset():
     pydirectinput.keyUp('d')
 
 
+def displayOnlyWhite(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_white = np.array([0, 0, 200])
+    upper_white = np.array([180, 55, 255])
+    mask = cv2.inRange(hsv, lower_white, upper_white)
+    result = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imshow('White Objects Only', result)
 
-def calculate_lanes(img, lines, color=[0, 255, 255], thickness=3):
+def removeHorizontalLines(lines):
+    if lines is None:
+        return lines
 
-    # if this fails, go with some default line
-    try:
+    filtered_lines = []
 
-        # finds the maximum y value for a lane marker 
-        # (since we cannot assume the horizon will always be at the same point.)
+    # Function to calculate the slope of a line
+    def calculate_slope(x1, y1, x2, y2):
+        if x2 - x1 == 0:  # To avoid division by zero
+            return float('inf')
+        else:
+            return (y2 - y1) / (x2 - x1)
 
-        ys = []  
-        for i in lines:
-            for ii in i:
-                ys += [ii[1],ii[3]]
-        min_y = min(ys)
-        max_y = 600
-        new_lines = []
-        line_dict = {}
-        slopes = []
-
-        for idx,i in enumerate(lines):
-            for xyxy in i:
-                # These four lines:
-                # modified from http://stackoverflow.com/questions/21565994/method-to-return-the-equation-of-a-straight-line-given-two-points
-                # Used to calculate the definition of a line, given two sets of coords.
-                x_coords = (xyxy[0],xyxy[2])
-                y_coords = (xyxy[1],xyxy[3])
-                A = vstack([x_coords,ones(len(x_coords))]).T
-                m, b = lstsq(A, y_coords)[0]
-
-                # Calculating our new, and improved, xs
-                if m == 0:
-                    x1 = min_y
-                    x2 = max_y
-                else:
-                    x1 = (min_y - b) / m
-                    x2 = (max_y - b) / m
-
-                slopes.append(m)
-                line_dict[idx] = [m,b,[int(x1), min_y, int(x2), max_y]]
-                new_lines.append([int(x1), min_y, int(x2), max_y])
-
-        final_lanes = {}
-
-        # print(min(slopes), max(slopes))
-
-        for idx in line_dict:
-            final_lanes_copy = final_lanes.copy()
-            m = line_dict[idx][0]
-            b = line_dict[idx][1]
-            line = line_dict[idx][2]
-            
-            if len(final_lanes) == 0:
-                final_lanes[m] = [ [m,b,line] ]
-                
-            else:
-                found_copy = False
-
-                for other_ms in final_lanes_copy:
-
-                    if not found_copy:
-                        if abs(other_ms*1.2) > abs(m) > abs(other_ms*0.8):
-                            if abs(final_lanes_copy[other_ms][0][1]*1.2) > abs(b) > abs(final_lanes_copy[other_ms][0][1]*0.8):
-                                final_lanes[other_ms].append([m,b,line])
-                                found_copy = True
-                                break
-                        else:
-                            final_lanes[m] = [ [m,b,line] ]
-
-        line_counter = {}
-
-        for lanes in final_lanes:
-            line_counter[lanes] = len(final_lanes[lanes])
-
-        top_lanes = sorted(line_counter.items(), key=lambda item: item[1])[::-1][:2]
-
-        lane1_id = top_lanes[0][0]
-        lane2_id = top_lanes[1][0]
-
-        def average_lane(lane_data):
-            x1s = []
-            y1s = []
-            x2s = []
-            y2s = []
-            for data in lane_data:
-                x1s.append(data[2][0])
-                y1s.append(data[2][1])
-                x2s.append(data[2][2])
-                y2s.append(data[2][3])
-            return int(mean(x1s)), int(mean(y1s)), int(mean(x2s)), int(mean(y2s)) 
-
-        l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
-        l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
-
-        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2], lane1_id, lane2_id
-
-
-    except Exception as e:
-        print(str(e))
+    # Iterate over the lines and filter based on the slope
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            slope = calculate_slope(x1, y1, x2, y2)
+            # print(f"Slope: {slope}")
+            if abs(slope) > 0.1:  # Consider absolute value to handle negative slopes
+                filtered_lines.append([[x1, y1, x2, y2]])
+    return filtered_lines
 
 
 def process_img(image):
@@ -144,21 +78,22 @@ def process_img(image):
     # convert to gray
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # edge detection
-    processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    processed_img =  cv2.Canny(processed_img, threshold1 = 150, threshold2=250)
     
     processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
     
-    vertices = np.array([[10,500],[10,350],[400, 250],[800,350],[800,500]], np.int32)
+    vertices = np.array([[220,550], [220, 380], [10, 380], [10,350],[300, 280], [500, 280],[800,350],[800,550], [400, 500]], np.int32)
 
     processed_img = roi(processed_img, [vertices])
+     
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 200, np.array([]), 150, 5)
 
-    # more info: http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
-    #                                     rho   theta   thresh  min length, max gap:        
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]),      100,       5)
+    lines = removeHorizontalLines(lines)
+
     m1 = 0
     m2 = 0
     try:
-        l1, l2, m1, m2 = calculate_lanes(original_image,lines)
+        l1, l2, m1, m2 = draw_lanes(original_image,lines)
         cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0,255,0], 20)
         cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0,255,0], 20)
     except Exception as e:
@@ -168,8 +103,7 @@ def process_img(image):
         for coords in lines:
             coords = coords[0]
             try:
-                cv2.line(processed_img, (coords[0], coords[1]), (coords[2], coords[3]), [255,0,0], 2)
-                
+                cv2.line(processed_img, (coords[0], coords[1]), (coords[2], coords[3]), [255,0,0], 3)
                 
             except Exception as e:
                 print(str(e))
@@ -184,17 +118,17 @@ def roi(img, vertices):
     masked = cv2.bitwise_and(img, mask)
     return masked
 
-for i in list(range(4))[::-1]:
+for i in list(range(2))[::-1]:
     print(i+1)
     time.sleep(1)
 
 last_time = time.time()
 while True:
-    screen =  np.array(ImageGrab.grab(bbox=(0, 30, 800, 630)))
+    screen = grab_screen(region=(0, 30, 800, 630))
     print('Frame took {} seconds'.format(time.time()-last_time))
     last_time = time.time()
 
-    new_screen,original_image, m1, m2 = process_img(screen)
+    new_screen, original_image, m1, m2 = process_img(screen)
     cv2.imshow('window', new_screen)
     cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
 
